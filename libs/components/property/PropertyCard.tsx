c4969837a06acd66ac3660cd044874890290f57e
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2025-01-02 19:40:29 +0900

    fix: property page fixed

--- libs/components/property/PropertyCard.tsx
+++ libs/components/property/PropertyCard.tsx
@@ -1,116 +1,77 @@
-import React from "react";
-import { Stack, Typography, Box } from "@mui/material";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { Favorite, RemoveRedEye } from "@mui/icons-material";
+import { Box, Divider, IconButton, Stack, Typography } from "@mui/material";
 import Link from "next/link";
-import IconButton from "@mui/material/IconButton";
-import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import React from "react";
+
 const PropertyCard = () => {
+  const imagePath: string = "/img/banner/header1.svg";
+
   return (
     <Stack className="card-config">
       <Stack className="top">
-        <Box
-          className={"card-img"}
-          style={{
-            backgroundImage: `url("/img/banner/types/house.webp")`,
-          }}
-        >
-          <div
-            style={{
-              position: "absolute",
-              padding: "8px 12px",
-              background: "#fff",
-              margin: "140px 20px",
-              borderRadius: "6px",
-              fontWeight: "600",
-            }}
-          >
-            $430000
-          </div>
+        <Link href={{ pathname: "/property/detail", query: { id: "demo1" } }}>
+          <img src={imagePath} alt="" />
+        </Link>
+        <Box className="top-badge">
+          <img src="/img/icons/electricity.svg" alt="" />
+          <Typography color={"#fff !important"}>TOP</Typography>
+        </Box>
+        <Box className="price-box">
+          <Typography>$250000</Typography>
         </Box>
       </Stack>
+
       <Stack className="bottom">
         <Stack className="name-address">
-          <Typography>Kingston Villas</Typography>
+          <Stack className="name">
+            <Link
+              href={{ pathname: "/property/detail", query: { id: "demo2" } }}
+            >
+              <Typography>Kingstone apartments</Typography>
+            </Link>
+          </Stack>
+          <Stack className="address">
+            <Typography>Busan, City Center 200-5</Typography>
+          </Stack>
+        </Stack>
+        <Stack className="options">
+          <Stack className="option">
+            <img src="/img/icons/bed.svg" alt="" />
+            <Typography>3 bed</Typography>
+          </Stack>
+          <Stack className="option">
+            <img src="/img/icons/room.svg" alt="" />
+            <Typography>7 rooms</Typography>
+          </Stack>
+          <Stack className="option">
+            <img src="/img/icons/expand.svg" alt="" />
+            <Typography>250 m2</Typography>
+          </Stack>
+        </Stack>
+        <Stack className="divider"></Stack>
+        <Stack className="type-buttons">
+          <Stack className="type">
+            <Typography sx={{ fontWeight: "500", fontSize: "13px" }}>
+              Rent
+            </Typography>
+            <Typography sx={{ fontWeight: "500", fontSize: "13px" }}>
+              Barter
+            </Typography>
+          </Stack>
+          <Stack className="buttons">
+            <IconButton color="default">
+              <RemoveRedEye />
+            </IconButton>
+            <Typography className="view-cnt">120</Typography>
+            <IconButton color="default">
+              <Favorite />
+            </IconButton>
+            <Typography className="view-cnt">200</Typography>
+          </Stack>
         </Stack>
       </Stack>
     </Stack>
   );
-  //     <Stack className="bottom">
-  //       <Stack className="name-address">
-  //         <Stack className="name">
-  //           <Link
-  //             href={{
-  //               pathname: "/property/detail",
-  //               query: { id: property?._id },
-  //             }}
-  //           >
-  //             <Typography>{property.propertyTitle}</Typography>
-  //           </Link>
-  //         </Stack>
-  //         <Stack className="address">
-  //           <Typography>
-  //             {property.propertyAddress}, {property.propertyLocation}
-  //           </Typography>
-  //         </Stack>
-  //       </Stack>
-  //       <Stack className="options">
-  //         <Stack className="option">
-  //           <img src="/img/icons/bed.svg" alt="" />{" "}
-  //           <Typography>{property.propertyBeds} bed</Typography>
-  //         </Stack>
-  //         <Stack className="option">
-  //           <img src="/img/icons/room.svg" alt="" />{" "}
-  //           <Typography>{property.propertyRooms} room</Typography>
-  //         </Stack>
-  //         <Stack className="option">
-  //           <img src="/img/icons/expand.svg" alt="" />{" "}
-  //           <Typography>{property.propertySquare} m2</Typography>
-  //         </Stack>
-  //       </Stack>
-  //       <Stack className="divider"></Stack>
-  //       <Stack className="type-buttons">
-  //         <Stack className="type">
-  //           <Typography
-  //             sx={{ fontWeight: 500, fontSize: "13px" }}
-  //             className={property.propertyRent ? "" : "disabled-type"}
-  //           >
-  //             Rent
-  //           </Typography>
-  //           <Typography
-  //             sx={{ fontWeight: 500, fontSize: "13px" }}
-  //             className={property.propertyBarter ? "" : "disabled-type"}
-  //           >
-  //             Barter
-  //           </Typography>
-  //         </Stack>
-  //         {!recentlyVisited && (
-  //           <Stack className="buttons">
-  //             <IconButton color={"default"}>
-  //               <RemoveRedEyeIcon />
-  //             </IconButton>
-  //             <Typography className="view-cnt">
-  //               {property?.propertyViews}
-  //             </Typography>
-  //             <IconButton
-  //               color={"default"}
-  //               onClick={() => likePropertyHandler(user, property?._id)}
-  //             >
-  //               {myFavorites ? (
-  //                 <FavoriteIcon color="primary" />
-  //               ) : property?.meLiked && property?.meLiked[0]?.myFavorite ? (
-  //                 <FavoriteIcon color="primary" />
-  //               ) : (
-  //                 <FavoriteBorderIcon />
-  //               )}
-  //             </IconButton>
-  //             <Typography className="view-cnt">
-  //               {property?.propertyLikes}
-  //             </Typography>
-  //           </Stack>
-  //         )}
-  //       </Stack>
-  //     </Stack>
-  //   </Stack>
 };
+
 export default PropertyCard;
