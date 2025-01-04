e2d423caa7d2a950b44db1b5d4abcdb111457d5d
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2025-01-04 18:52:09 +0900

    fix: BINGO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
@@ -10,50 +9,21 @@ import PopularProperties from "@/libs/components/homepage/PopularProperties";
 import Advertisement from "@/libs/components/homepage/Advertisement";
 import TopProperties from "@/libs/components/homepage/TopProperties";
 import TopAgents from "@/libs/components/homepage/TopAgents";
-import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
-import { useQuery } from "@apollo/client";
-import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
-  const device = useDeviceDetect();
-
-  const {
-    loading: getPropertiesLoading,
-    data: getPropertiesData,
-    error: getPropertiesError,
-    refetch: getPropertiesRefetch,
-  } = useQuery(GET_PROPERTIES, {
-    fetchPolicy: "network-only",
-    variables: {
-      input: {
-        page: 1,
-        limit: 5,
-        sort: "createdAt",
-        direction: "DESC",
-        search: {},
-      },
-    },
-  });
-
-  console.log("getPropertiesData => ", getPropertiesData);
-
-  if (device === "mobile") {
-    return <Stack>HOMEPAGE MOBILE</Stack>;
-  } else {
-    return (
-      <>
-        <Stack>
-          <Stack className='home-page'>
-            <TrendProperties />
-            <PopularProperties />
-            <Advertisement />
-            <TopProperties />
-            <TopAgents />
-          </Stack>
+  return (
+    <>
+      <Stack>
+        <Stack className='home-page'>
+          <TrendProperties />
+          <PopularProperties />
+          <Advertisement />
+          <TopProperties />
+          <TopAgents />
         </Stack>
-      </>
-    );
-  }
+      </Stack>
+    </>
+  );
 };
 
 export default withLayoutMain(Home);
