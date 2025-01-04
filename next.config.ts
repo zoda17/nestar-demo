4460808105f623c7db13fbce085f264c581ee81a
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2025-01-04 15:10:50 +0900

    fix: nextconfig error fixed

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig= {
   /* config options here */
   reactStrictMode: true,
 };
