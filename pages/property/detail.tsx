dd6d609991f394b13e0c4dad5f168c83899764c3
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2025-01-04 15:02:00 +0900

    fix: homePage fix

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -1,7 +1,8 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
 
-const PropertyDetail = () => {
+const PropertyDetail: NextPage = () => {
   return <Container>PROPERTY DETAIL</Container>;
 };
 
